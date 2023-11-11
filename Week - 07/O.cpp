14724ceb1098fdac4ac43a93fda8133efdc857e2
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2023-11-11 14:18:00 +0600

    Create O.cpp

---
+++ Week - 07/O.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace  std;
+using ll = long long;
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        cout << (n << 1) << '\n';
+    }
+    return 0;
+
+}
